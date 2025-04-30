2936ea50add0482c71ee303da2f821615d32771d
Leon <leonmonkeygamer@gmail.com>
2025-04-30 21:07:14 +0200

    Updated Main Page, added tailwindcss and daisyui

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1 className='flex-1/2'>Welcome to ChonkyCloud</h1>
+      <p>ChonkyCloud is a cloud storage solution that allows you to store and share files securely.</p>
+      <p>To get started, please sign up or log in.</p>
+      <p>If you have any questions, feel free to contact us.</p>
+    </div>
+  )
+}
+
+export default MainPage
