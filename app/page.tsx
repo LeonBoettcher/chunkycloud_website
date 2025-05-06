c22043ad8a09313f57bb3c9ed7a315ac622e80db
leon <leonmonkeygamer@gmail.com>
2025-05-06 19:10:15 +0200

    fixed front Page mobile View

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const MainPage = () => {
   return (
     <>
       <div
-        className="hero min-h-screen"
+        className="hero min-h-screen relative"
         style={{
           backgroundImage: "url(/images/Bluelix-swamp_and_mountain.png)",
         }}
@@ -17,15 +17,19 @@ const MainPage = () => {
         <div className="hero-content text-neutral-content text-center">
           <div>
             <div>
-              <h1 className="mb-5 text-7xl font-bold">Welcome to</h1>
-              <h1 className="mb-5 text-7xl font-bold">ChunkyCloud!</h1>
-              <p className="mb-5 text-2xl">
+              <h1 className="mb-5 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+                Welcome to
+              </h1>
+              <h1 className="mb-5 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+                ChunkyCloud!
+              </h1>
+              <p className="mb-5 text-lg sm:text-xl md:text-2xl">
                 A distributed rendering service for{" "}
                 <Link className="underline" href="https://chunky.lemaik.de/">
                   Chunky
                 </Link>
               </p>
-              <div className="pt-25 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              <div className="pt-10 px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
                 <HeroCards
                   Title="Create a new job"
                   Description="Upload your scene and render it on a distributed server farm."
@@ -45,7 +49,7 @@ const MainPage = () => {
             </div>
           </div>
         </div>
-        <div className="absolute bottom-4 right-4 z-10">
+        <div className="absolute bottom-2 left-2 text-xs text-white/70">
           <Link href="https://chunky-dev.github.io/gallery/">
             "swamp and mountain" by Bluelix
           </Link>
--- components/Index/HeroCards.tsx
+++ components/Index/HeroCards.tsx
@@ -9,7 +9,7 @@ interface Variables {
 const HeroCards = ({ Title, Description, Link }: Variables) => {
   return (
     <a href={Link} className="block">
-      <div className="card w-96 rounded-xl border border-base-200 bg-base-100/80 shadow-md transition-transform duration-300 ease-in-out hover:scale-101 hover:shadow-xl hover:bg-base-200/70 cursor-pointer">
+      <div className="card w-full max-w-sm mx-auto rounded-xl border border-base-200 bg-base-100/80 shadow-md transition-transform duration-300 ease-in-out hover:scale-101 hover:shadow-xl hover:bg-base-200/70 cursor-pointer">
         <div className="card-body space-y-2">
           <h2 className="card-title text-2xl font-semibold">{Title}</h2>
           <p className="text-base text-neutral-content">{Description}</p>
